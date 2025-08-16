6a52130f44b0b251a4404138dcbeacfcb9a83c5d
Joost <git@jstt.me>
2025-08-16 12:01:46 +0200

    Update demo

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,25 @@
+import { useEvent } from "expo";
 import { useVideoPlayer, VideoView } from "expo-video";
 
-const testHlsUrl =
-  "https://demo.unified-streaming.com/k8s/features/stable/no-handler-origin/tears-of-steel/tears-of-steel-trickplay.m3u8";
+const hlsPlaylistUrl =
+  "https://5b44cf20b0388.streamlock.net:8443/vod/smil:hls-maudios-prod.smil/playlist.m3u8";
+
+const mp4VideoUrl = "https://mirror.selfnet.de/CCC/congress/2019/h264-hd/36c3-11235-eng-deu-fra-36C3_Infrastructure_Review_hd.mp4";
 
 export default function Video() {
   const player = useVideoPlayer(
     {
-      uri: testHlsUrl,
-      metadata: {
-        title: "Tears of Steel",
-        artist: "Blender Foundation",
-      },
+      uri: hlsPlaylistUrl,
     },
     (player) => {
       player.play();
       player.allowsExternalPlayback = true;
       player.staysActiveInBackground = true;
-      player.showNowPlayingNotification = true;
     }
   );
+
+  const data = useEvent(player, "availableAudioTracksChange");
+  console.log("availableAudioTracksChange", data);
 
   return (
     <VideoView
