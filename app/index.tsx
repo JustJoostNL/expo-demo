64262145e7c56023d72f5195fea35aa3c7176484
Joost <git@jstt.me>
2025-09-21 17:31:43 +0200

    Add share example

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import * as Notifications from "expo-notifications";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+import * as Sharing from "expo-sharing";
 
 export default function Index() {
-  const [data, setData] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    (async () => {
-      console.log("getting token");
-      const data = await Notifications.getDevicePushTokenAsync();
-      console.log("this log never shows because the function does not resolve");
-      setData(data.data);
-    })();
-  }, []);
+  const handleShare = async () => {
+    Sharing.shareAsync("https://google.com").catch((error) => {
+      Alert.alert("Error", error.message);
+    });
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: "red" }}>{data ?? "no data"}</Text>
+      <TouchableOpacity onPress={handleShare} style={{ marginBottom: 20 }}>
+        <Text style={{ color: "red" }}>Click here to share</Text>
+      </TouchableOpacity>
     </View>
   );
 }
